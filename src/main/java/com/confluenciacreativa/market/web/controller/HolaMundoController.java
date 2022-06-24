b19ca4e66b0e348e72308f35d142e536179f7778
lauradillonar <lauradillonar@gmail.com>
2022-06-24 10:25:41 -0300

    Creando la estructura del proyecto

--- src/main/java/com/confluenciacreativa/market/web/controller/HolaMundoController.java
+++ src/main/java/com/confluenciacreativa/market/web/controller/HolaMundoController.java
@@ -1,4 +1,4 @@
-package com.confluenciacreativa.market;
+package com.confluenciacreativa.market.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
