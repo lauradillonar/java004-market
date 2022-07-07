2d139fd970f594af097b06c0449b43f0c7807185
lauradillonar <lauradillonar@gmail.com>
2022-07-07 18:03:10 -0300

    Creando Entity cuando su clave primaria es compuesta

---
+++ src/main/java/com/confluenciacreativa/market/persistence/entity/ComprasProducto.java
@@ -0,0 +1,36 @@
+package com.confluenciacreativa.market.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "COMPRAS_PRODUCTOS")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+    private Double total;
+
+    @Column(columnDefinition = "TINYINT")
+    private Boolean estado;
+
+    public ComprasProductoPK getId() {return id;}
+
+    public void setId(ComprasProductoPK id) {this.id = id;}
+
+    public Integer getCantidad() {return cantidad;}
+
+    public void setCantidad(Integer cantidad) {this.cantidad = cantidad;}
+
+    public Double getTotal() {return total;}
+
+    public void setTotal(Double total) {this.total = total;}
+
+    public Boolean getEstado() {return estado;}
+
+    public void setEstado(Boolean estado) {this.estado = estado;}
+}
